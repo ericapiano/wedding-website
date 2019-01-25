aafb56dae44e5526cc5658adf8ebaadb42aecdf5
Dhanashree <dvphalak@gmail.com>
2019-01-24 21:22:08 -0500

    login and guestlist

--- client/src/Pages/GuestList/GuestList.js
+++ client/src/Pages/GuestList/GuestList.js
@@ -1,11 +1,38 @@
 import React, { Component } from "react";
 import API from '../../utils/API';
+import './GuestList.css'
 
 class GuestList extends Component {
-  state ={
-    GuestName:"",
+  state = {
+    GuestName: "",
+    showBtns: true,
 
   }
+  guests  = (x)=> {
+    console.log(x)
 
+
+    
+  }
+  events = (y) => {
+    console.log(y)
+  }
+  render() {
+    
+    return (
+      <div>
+        <h3>Select an Action</h3>
+        <span>
+          <button className="cursor-style" onClick={() => this.guests("word") }>Guest List</button> 
+        </span>
+        <span>
+          <button className="cursor-style" onClick={() => this.events("events") }>Events List</button>
+        </span>
+      </div>
+    )
+
+
+  }
 }
+
 export default GuestList;
